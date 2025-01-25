d0c244c90994a257dffbabd9352f907fc3fefee0
innerpixel <ned.gabrielc@gmail.com>
2025-01-25 16:23:39 +0200

    deploy

---
+++ assets/AboutView-S36BPORw.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./index-CtruSPQ5.js";import{c as t,a as s,o as a}from"./vendor-Dmn3DHVG.js";const r={},n={class:"about"};function c(i,e){return a(),t("div",n,e[0]||(e[0]=[s("h1",null,"This is an about page",-1)]))}const l=o(r,[["render",c]]);export{l as default};
+//# sourceMappingURL=AboutView-S36BPORw.js.map
